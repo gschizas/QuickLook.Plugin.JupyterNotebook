9fa34cf950f977ecbcfb7cebf9be17b52abed055
George Schizas <gschizas@gmail.com>
2018-10-24 13:35:01 +0300

    Actually do the translation

--- QuickLook.Plugin.JupyterNotebook/Plugin.cs
+++ QuickLook.Plugin.JupyterNotebook/Plugin.cs
@@ -16,17 +16,20 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Threading;
 using QuickLook.Common.Plugin;
 
 namespace QuickLook.Plugin.JupyterNotebook
 {
-    public class Plugin: IViewer
+    public class Plugin : IViewer
     {
         private WebpagePanel _panel;
+        private string tempHtmlPath;
 
         public int Priority => 0;
 
@@ -49,16 +52,64 @@ namespace QuickLook.Plugin.JupyterNotebook
         {
             _panel = new WebpagePanel();
             context.ViewerContent = _panel;
+
             context.Title = Path.IsPathRooted(path) ? Path.GetFileName(path) : path;
-            
-            _panel.LoadFile(path);
+            tempHtmlPath = ConvertNotebookToHtml(path);
+            _panel.LoadFile(tempHtmlPath);
             _panel.Dispatcher.Invoke(() => { context.IsBusy = false; }, DispatcherPriority.Loaded);
+        }
+
+        private string ConvertNotebookToHtml(string path)
+        {
+            var outputFilename = System.IO.Path.GetTempPath() + Guid.NewGuid().ToString() + ".html";
+            var exePath = @"C:\Program Files\Python37\Scripts\pipenv.exe";
+            var directory = System.IO.Path.GetDirectoryName(path);
+
+            var paramsBuilder = new StringBuilder();
+            paramsBuilder.Append("run jupyter nbconvert ");
+            paramsBuilder.Append("\"" + path + "\"");
+            paramsBuilder.Append(" --output ");
+            paramsBuilder.Append("\"" + outputFilename + "\"");
+
+            var si = new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                FileName = exePath,
+                Arguments = paramsBuilder.ToString(),
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                WorkingDirectory = directory
+            };
+
+            using (var process = new Process())
+            {
+                process.StartInfo = si;
+                process.Start();
+
+                if (process.WaitForExit(10 * 1000))
+                {
+                    return outputFilename;
+                }
+
+                // timed out, but maybe there's hope yet
+                process.Kill();
+                if (!File.Exists(outputFilename))
+                {
+                    throw new TimeoutException();
+                }
+            }
+
+            return outputFilename;
         }
 
         public void Cleanup()
         {
             _panel?.Dispose();
             _panel = null;
+
+            File.Delete(tempHtmlPath);
+            tempHtmlPath = null;
         }
     }
 }
